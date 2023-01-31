f319998ae648a97dce04809e879187c4a6a52534
Aleksei Kuzmichev <a.kuzmichev@corp.mail.ru>
2023-01-31 18:43:50 +0600

    build

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,11 @@
-export { getCode, getCounterValue, getHMAC, getKey, getCounter } from "./hotp";
+export {
+  getCode,
+  getCounterValue,
+  getHMAC,
+  getKey,
+  getCounter,
+  SupportedEncodings,
+} from "./hotp";
 export { BASE32_ALPHABET, BASE32_PADDING } from "./converters/alphabet";
 export { convert, to_u8a, from_u8a } from "./converters";
 export { getTOTP } from "./getTOTP";
