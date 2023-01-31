9a187e143a30f62a8413c0fe039420638824a249
Aleksei Kuzmichev <a.kuzmichev@corp.mail.ru>
2023-01-31 18:39:21 +0600

    build

--- dist/hotp/index.d.ts
+++ dist/hotp/index.d.ts
@@ -1,5 +1,5 @@
 export { getCode } from "./getCode";
-export { getKey } from "./getKey";
+export { getKey, SupportedEncodings } from "./getKey";
 export { getCounterValue } from "./getCounterValue";
 export { getCounter } from "./getCounter";
 export { getHMAC } from "./getHMAC";
